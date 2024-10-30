29edfc3b703a516203ae33efc73b89f46b6c1234
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-30 22:00:15 +0600

    patient table make responsive

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -6,71 +6,52 @@ const Patient = () => {
   const navigate = useNavigate();
   return (
     <>
-      <section className="container">
-        <div className=" mt-8 mb-10 ">
-          <input
-            type="text"
-            name=""
-            id=""
-            className="border w-[30%] p-3 rounded-md border-black"
-            placeholder="search patiens"
-          />
-        </div>
+      <div className="mt-3 md:mx-auto w-full">
+        <input
+          type="text"
+          name=""
+          id=""
+          className="border sm:w[50%] md:w-[60%] sm:mx-auto placeholder:text-white p-2 flex mx-auto rounded-md border-black"
+          placeholder="search patiens"
+        />
+      </div>
 
-        <PatientSummaryCard />
-        <div className="border border-gray-300 mx-2 mb-2">
-          <div className="flex">
-            <div className=" mt-8 mb-10 w-[20%] ">
-              <input
-                type="text"
-                name=""
-                id=""
-                className="border border-gray-300 p-3 rounded-md "
-                placeholder="search patiens"
-              />
-            </div>
-            <div className="mt-8 ">
-              <select className="border border-gray-300 bg-white h-12 w-48 rounded-md">
-                <option value="">Sort by Patiens</option>
-                <option value="small">Oldest Patients</option>
-                <option value="medium">Newest Patients</option>
-              </select>
-            </div>
-            <div className="mt-8 mx-7 w-48 ">
-              <select className="border border-gray-300 bg-white h-12 w-48 rounded-md">
-                <option value="">Gender</option>
-                <option value="small">Male</option>
-                <option value="medium">Female</option>
-              </select>
-            </div>
-            <div className=" mt-8 mb-10 w-[20%] ">
-              <input
-                type="text"
-                name=""
-                id=""
-                className="border border-gray-300 p-3 rounded-md "
-                placeholder="search patiens"
-              />
-            </div>
-            <div className="border border-gray-300 h-16 mt-7 w-[20%] bg-green-500 relative hover:bg-gray-400">
-              <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer">
-                FILTER
-              </button>
-            </div>
+      {/*<PatientSummaryCard /> */}
+
+      <div className="container mb-5 mt-5">
+        <div className="flex flex-col md:flex-row gap-y-4 w-full justify-evenly">
+          <div className="basis-[33%]">
+            <select className="flex-1 p-4 mx-2 w-[50%]">
+              <option value="px-0">Sort by Patiens</option>
+              <option value="small px-0">Oldest Patients</option>
+              <option value="medium px-0">Newest Patients</option>
+            </select>
           </div>
-          <PatientTable />
-        </div>
-        <div className="flex justify-end items-end mt-12 mb-5 relative">
-          <div className="relative h-16 w-16">
-            <button
-              className="flex items-center justify-center border h-18 w-18 bg-blue-600 rounded-full font-bold absolute inset-0 m-auto bounce-animation"
-              onClick={()=>navigate("/patient-entry")}
-            >
-              +
-            </button>
+          <div className="basis-[33%]">
+            <select className="flex-1 p-4 mx-2 w-[50%]">
+              <option value="">Gender</option>
+              <option value="small">Male</option>
+              <option value="medium">Female</option>
+            </select>
           </div>
+
+          <button className="p-4 border ml-1 mr-1 md:m-0 text-black bg-slate-400 hover:bg-orange-400 basis-[33%] ">
+            Filter
+          </button>
         </div>
-      </section>
+      </div>
+
+      <PatientTable />
+      <div className="flex justify-end items-end mt-12 mb-5 relative">
+        <div className="relative h-16 w-16">
+          <button
+            className="flex items-center justify-center border h-18 w-18 bg-blue-600 rounded-full font-bold absolute inset-0 m-auto bounce-animation"
+            onClick={() => navigate("/patient-entry")}
+          >
+            +
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/PatientTable.jsx
+++ src/components/PatientTable.jsx
@@ -1,126 +1,156 @@
-import jillur from '../assets/jillur.jpg'
+import jillur from "../assets/jillur.jpg";
 const PatientTable = () => {
   return (
     <>
-      <div className='container mx-auto overflow-x-auto'>
-        <table className="container min-w-full border border-collapse mb-2">
-            <thead className="container">
-                <tr className="">
-                    <th className="">#</th>
-                    <th className="">Patient</th>
-                    <th className="px-10">Created At</th>
-                    <th className="px-6">Gender</th>
-                    <th className="px-6">Blood Group</th>
-                    <th className="px-6">Age</th>
-                    <th className="px-6">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className='border-b-2'>
-                    <td className="text-center">1</td>
-                    <td className="text-center">
-                        <div className=' flex justify-end '>
-                            <div className='flex items-center justify-center'>
-                            <img src={jillur} alt="" className='h-12 w-12 rounded-full ' />
-                            </div>
-                            <div className='mx-4'>
-                            <h1>Md. Jillur Rahman</h1>
-                            <h2>+8801611780984</h2>
-                            </div>
-                        </div>
-                    </td>
-                    <td className="px-10 text-center">20 Augest 2021</td>
-                    <td className="px-6 text-center">Male</td>
-                    <td className="px-6 text-center">B+</td>
-                    <td className="px-6 text-center">27</td>
-                    <td className="px-6 text-center"><span className="font-bold cursor-pointer">...</span></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr className='border-b-2'>
-                    <td className="text-center">1</td>
-                    <td className="text-center">
-                        <div className=' flex justify-end '>
-                            <div className='flex items-center justify-center'>
-                            <img src={jillur} alt="" className='h-12 w-12 rounded-full ' />
-                            </div>
-                            <div className='mx-4'>
-                            <h1>Md. Jillur Rahman</h1>
-                            <h2>+8801611780984</h2>
-                            </div>
-                        </div>
-                    </td>
-                    <td className="px-10 text-center">20 Augest 2021</td>
-                    <td className="px-6 text-center">Male</td>
-                    <td className="px-6 text-center">B+</td>
-                    <td className="px-6 text-center">27</td>
-                    <td className="px-6 text-center"><span className="font-bold cursor-pointer">...</span></td>
-                </tr>
-                
-            </tbody>
-            <tbody>
-                <tr className='border-b-2'>
-                    <td className="text-center">1</td>
-                    <td className="text-center">
-                        <div className=' flex justify-end '>
-                            <div className='flex items-center justify-center'>
-                            <img src={jillur} alt="" className='h-12 w-12 rounded-full ' />
-                            </div>
-                            <div className='mx-4'>
-                            <h1>Md. Jillur Rahman</h1>
-                            <h2>+8801611780984</h2>
-                            </div>
-                        </div>
-                    </td>
-                    <td className="px-10 text-center">20 Augest 2021</td>
-                    <td className="px-6 text-center">Male</td>
-                    <td className="px-6 text-center">B+</td>
-                    <td className="px-6 text-center">27</td>
-                    <td className="px-6 text-center"><span className="font-bold cursor-pointer">...</span></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr className='border-b-2'>
-                    <td className="text-center">1</td>
-                    <td className="text-center">
-                        <div className=' flex justify-end '>
-                            <div className='flex items-center justify-center'>
-                            <img src={jillur} alt="" className='h-12 w-12 rounded-full ' />
-                            </div>
-                            <div className='mx-4'>
-                            <h1>Md. Jillur Rahman</h1>
-                            <h2>+8801611780984</h2>
-                            </div>
-                        </div>
-                    </td>
-                    <td className="px-10 text-center">20 Augest 2021</td>
-                    <td className="px-6 text-center">Male</td>
-                    <td className="px-6 text-center">B+</td>
-                    <td className="px-6 text-center">27</td>
-                    <td className="px-6 text-center"><span className="font-bold cursor-pointer">...</span></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td className="text-center">1</td>
-                    <td className="text-center">
-                        <div className=' flex justify-end '>
-                            <div className='flex items-center justify-center'>
-                            <img src={jillur} alt="" className='h-12 w-12 rounded-full ' />
-                            </div>
-                            <div className='mx-4'>
-                            <h1>Md. Jillur Rahman</h1>
-                            <h2>+8801611780984</h2>
-                            </div>
-                        </div>
-                    </td>
-                    <td className="px-10 text-center">20 Augest 2021</td>
-                    <td className="px-6 text-center">Male</td>
-                    <td className="px-6 text-center">B+</td>
-                    <td className="px-6 text-center">27</td>
-                    <td className="px-6 text-center"><span className="font-bold cursor-pointer">...</span></td>
-                </tr>
-            </tbody>
+      <div className="container mx-auto overflow-x-auto">
+        <table className="border w-full border-collapse  mb-2">
+          <thead className="">
+            <tr className="">
+              <th className="border border-black">ID</th>
+              <th className="border border-black">Patient</th>
+              <th className="border border-black">Created At</th>
+              <th className="border border-black">Gender</th>
+              <th className="border border-black">Blood Group</th>
+              <th className="border border-black">Age</th>
+              <th className="border border-black">Actions</th>
+            </tr>
+          </thead>
+          
+          <tbody className="">
+            <tr className="border">
+              <td className="text-center">1</td>
+              <td className="text-center">
+                <div className=" flex justify-end ">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={jillur}
+                      alt=""
+                      className="h-12 w-12 rounded-full "
+                    />
+                  </div>
+                  <div className="mx-4">
+                    <h1>Md. Jillur Rahman</h1>
+                    <h2>+8801611780984</h2>
+                  </div>
+                </div>
+              </td>
+              <td className="px-10 text-center">20 Augest 2021</td>
+              <td className="px-6 text-center">Male</td>
+              <td className="px-6 text-center">B+</td>
+              <td className="px-6 text-center">27</td>
+              <td className="px-6 text-center">
+                <span className="font-bold cursor-pointer">...</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr className="border-b-2">
+              <td className="text-center">1</td>
+              <td className="text-center">
+                <div className=" flex justify-end ">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={jillur}
+                      alt=""
+                      className="h-4 w-8 md:h-12 md:w-12  rounded-full "
+                    />
+                  </div>
+                  <div className="mx-4">
+                    <h1>Md. Jillur Rahman</h1>
+                    <h2>+8801611780984</h2>
+                  </div>
+                </div>
+              </td>
+              <td className="px-10 text-center">20 Augest 2021</td>
+              <td className="px-6 text-center">Male</td>
+              <td className="px-6 text-center">B+</td>
+              <td className="px-6 text-center">27</td>
+              <td className="px-6 text-center">
+                <span className="font-bold cursor-pointer">...</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr className="border-b-2">
+              <td className="text-center">1</td>
+              <td className="text-center">
+                <div className=" flex justify-end ">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={jillur}
+                      alt=""
+                      className="h-12 w-12 rounded-full "
+                    />
+                  </div>
+                  <div className="mx-4">
+                    <h1>Md. Jillur Rahman</h1>
+                    <h2>+8801611780984</h2>
+                  </div>
+                </div>
+              </td>
+              <td className="px-10 text-center">20 Augest 2021</td>
+              <td className="px-6 text-center">Male</td>
+              <td className="px-6 text-center">B+</td>
+              <td className="px-6 text-center">27</td>
+              <td className="px-6 text-center">
+                <span className="font-bold cursor-pointer">...</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr className="border-b-2">
+              <td className="text-center">1</td>
+              <td className="text-center">
+                <div className=" flex justify-end ">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={jillur}
+                      alt=""
+                      className="h-12 w-12 rounded-full "
+                    />
+                  </div>
+                  <div className="mx-4">
+                    <h1>Md. Jillur Rahman</h1>
+                    <h2>+8801611780984</h2>
+                  </div>
+                </div>
+              </td>
+              <td className="px-10 text-center">20 Augest 2021</td>
+              <td className="px-6 text-center">Male</td>
+              <td className="px-6 text-center">B+</td>
+              <td className="px-6 text-center">27</td>
+              <td className="px-6 text-center">
+                <span className="font-bold cursor-pointer">...</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td className="text-center">1</td>
+              <td className="text-center">
+                <div className=" flex justify-end ">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={jillur}
+                      alt=""
+                      className="h-12 w-12 rounded-full "
+                    />
+                  </div>
+                  <div className="mx-4">
+                    <h1>Md. Jillur Rahman</h1>
+                    <h2>+8801611780984</h2>
+                  </div>
+                </div>
+              </td>
+              <td className="px-10 text-center">20 Augest 2021</td>
+              <td className="px-6 text-center">Male</td>
+              <td className="px-6 text-center">B+</td>
+              <td className="px-6 text-center">27</td>
+              <td className="px-6 text-center">
+                <span className="font-bold cursor-pointer">...</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </>
