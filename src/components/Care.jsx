0bbe1e9661a4833bae761852ed0bc9678038d421
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-11-16 12:49:48 +0600

    Care Page Data Load from API

--- src/components/Care.jsx
+++ src/components/Care.jsx
@@ -5,26 +5,43 @@ import DoctorCard from "./DoctorCard";
 
 const Care = () => {
   const [doctorData, setDoctorData] = useState([]);
-
+  const[loading,setLoading]=useState(false)
+  const[page,setPage]=useState(3)
   console.log(doctorData?.data);
 
   useEffect(() => {
     const fetchDoctorData = async () => {
+      setLoading(true)
       try {
         const response = await axios.get(
           `${
             import.meta.env.VITE_SERVER_BASE_URL
-          }/members/local?page=2&size=10&filter=role:doctor&expand=doctor.*`
+          }/members/local?page=${page}&size=16&filter=role:doctor&expand=doctor.*`
         );
         if (response.status === 200) {
           setDoctorData(response.data);
+          
         }
       } catch (error) {
         console.log(error);
       }
+      finally{
+        setLoading(false)
+      }
     };
     fetchDoctorData();
-  }, []);
+  }, [page]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+        <span className="loading loading-spinner text-yellow-400 w-20 h-20"></span>
+        <h2 className="text-2xl font-bold text-yellow-400 mt-4">
+          Please wait! Data is loading...
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <section className="container bg-black mt-2">
--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,166 +1,50 @@
 import { Link } from "react-router-dom";
 import Doctor1 from "../assets/doctor1.jpg";
-import Doctor2 from "../assets/doctor2.jpg";
-import Doctor3 from "../assets/doctor3.jpeg";
-import Doctor4 from "../assets/doctor4.jpeg";
-import Doctor5 from "../assets/doctor5.jpeg";
-import Doctor6 from "../assets/doctor6.jpeg";
-import Doctor7 from "../assets/doctor7.jpeg";
+// import Doctor2 from "../assets/doctor2.jpg";
+// import Doctor3 from "../assets/doctor3.jpeg";
+// import Doctor4 from "../assets/doctor4.jpeg";
+// import Doctor5 from "../assets/doctor5.jpeg";
+// import Doctor6 from "../assets/doctor6.jpeg";
+// import Doctor7 from "../assets/doctor7.jpeg";
 
-const DoctorCard = ({doctorData}) => {
+const DoctorCard = ({ doctorData }) => {
+  console.log(doctorData);
 
-  console.log(doctorData)
-  
   return (
     <>
-    <div className="flex w-full basis-[80%] md:basis-[70%] sm:basis-[60%] mx-auto">
-          <Link to="/doctor-details">
-           {
-            doctorData.map((doctor,i)=>{
-             return <div key={i} className="grid w-full justify-center items-center mx-0  md:mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2  ">
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor1}
-                  alt=""
-                  className="w-full md:h-48 object-cover"
-                />
-                
-                <div className="bg-slate-900 text-white">
-                  <h1>{doctor.id}</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1 ">
-                <img
-                  src={Doctor2}
-                  alt=""
-                  className="w-full md:h-48 object-cover"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor3}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1 overflow-hidden">
-                <img
-                  src={Doctor4}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor5}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor6}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor7}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor2}
-                  alt=""
-                  className="w-full md:h-48 object-cover"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-              <div className="w-full md:basis-[26%] bg-gray-700 mb-1">
-                <img
-                  src={Doctor6}
-                  alt=""
-                  className="w-full md:h-48 object-fit"
-                />
-                <div className="bg-slate-900 text-white">
-                  <h1>Dr. Md. Yusuf Ali</h1>
-                  <h2>yusuf.ali@gmail.com</h2>
-                  <p>Avaiable all day</p>
-                  <h2>8 Years of Experience</h2>
-                  <h2 className="text-md font-semibold mt-4">Rating</h2>
-                  <p className="text-yellow-500">★★★★☆</p>
-                </div>
-              </div>
-            </div>
-            })
-           }
-          </Link>
-        </div>
-    </>
-  )
-}
+      <div className="flex w-full basis-[80%] md:basis-[70%] sm:basis-[60%] mx-auto">
+        <Link to="/doctor-details">
+          {
+            <div className="grid w-full justify-center items-center md:grid-cols-3 lg:grid-cols-4 gap-3  ">
+              {doctorData.map((doctor, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="w-full bg-gray-700 mb-1"
+                  >
+                    <img
+                      src={Doctor1}
+                      alt=""
+                      className="w-full md:h-48 object-cover"
+                    />
 
-export default DoctorCard
+                    <div className="bg-slate-900  md:p-5 text-white">
+                      <h1>Dr. Md. Yusuf Ali</h1>
+                      <h2>yusuf.ali@gmail.com</h2>
+                      <p>Avaiable all day</p>
+                      <h2>8 Years of Experience</h2>
+                      <h2 className="text-md font-semibold mt-4">Rating</h2>
+                      <p className="text-yellow-500">★★★★☆</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          }
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default DoctorCard;
